1ee873ee25f4b0ac4d5accae70d56e6067f76583
Dani M <daniel.m.aldea@hotmail.com>
2022-01-21 22:44:30 +0100

    moved job to its own file and added unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate termion;
 
-use std::{io::Write, path::PathBuf};
+mod job;
+
+use crate::job::Job;
+use std::io::Write;
 use termion::input::TermRead;
 
 fn prompt(stdout: &mut std::io::StdoutLock) {
@@ -8,52 +11,17 @@ fn prompt(stdout: &mut std::io::StdoutLock) {
     stdout.flush().unwrap();
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Command<'a> {
-    cmd: String,
-    args: Vec<&'a str>,
-}
-
-impl<'a> Command<'a> {
-    fn new(cmd: String, args: Vec<&'a str>) -> Self {
-        Self { cmd, args }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Job<'a> {
-    cmds: Vec<Command<'a>>,
-    input: Option<PathBuf>,
-    output: Option<PathBuf>,
-}
-
-impl<'a> Job<'a> {
-    fn new(input: &'a str) -> Self {
-        let mut commands: Vec<Command<'a>> = Vec::new();
-        for command in input.trim().split('|') {
-            let tokens: Vec<&str> = command.trim().split_whitespace().collect();
-            commands.push(Command::new(tokens[0].to_string(), tokens[1..].to_vec()));
-        }
-
-        Self {
-            cmds: commands,
-            input: None,
-            output: None,
-        }
-    }
-
-    fn execute(&self) {
-        // let output = std::process::Command::new(line).output();
-        // match output {
-        //     Ok(output) => {
-        //         stdout.write_all(&output.stdout).expect("Error writing");
-        //         stdout.flush().unwrap();
-        //     }
-        //     Err(msg) => {
-        //         panic!("{}", msg);
-        //     }
-        // }
-    }
+fn execute() {
+    // let output = std::process::Command::new(line).output();
+    // match output {
+    //     Ok(output) => {
+    //         stdout.write_all(&output.stdout).expect("Error writing");
+    //         stdout.flush().unwrap();
+    //     }
+    //     Err(msg) => {
+    //         panic!("{}", msg);
+    //     }
+    // }
 }
 
 fn main() {
